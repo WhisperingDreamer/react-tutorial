87615db150767bf1c19a40867d92d972844c1f4d
WhisperingDreamer <melnikovivan100@gmail.com>
2024-09-15 16:26:16 +0300

    Реализована функция редактирования пользователей

--- src/App.js
+++ src/App.js
@@ -34,13 +34,14 @@ class App extends React.Component {
     }
     this.addUser = this.addUser.bind(this)
     this.deleteUser = this.deleteUser.bind(this)
+    this.editUser = this.editUser.bind(this)
   }
 
   render() {
   return (<div>
       <Header title='Список пользователей' />
       <main>
-        <Users users={this.state.users} onDelete={this.deleteUser} />
+        <Users users={this.state.users} onEdit={this.editUser} onDelete={this.deleteUser} />
       </main>
       <aside>
         <AddUser onAdd={this.addUser} />
@@ -51,6 +52,15 @@ class App extends React.Component {
   deleteUser(id) {
     this.setState({
       users: this.state.users.filter((el) => el.id !== id)
+    })
+  }
+
+  editUser(user) {
+    let allUsers = this.state.users
+    allUsers[user.id - 1] = user
+
+    this.setState({users: []}, () => {
+      this.setState({users: [...allUsers] })
     })
   }
 
